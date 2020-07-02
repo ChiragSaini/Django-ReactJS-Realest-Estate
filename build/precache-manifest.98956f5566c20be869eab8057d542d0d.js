166cc1a275c7c6a28d53aaf587d48a16241c4bce
Chirag Saini <chiragsaini7777@gmail.com>
2020-07-02 11:53:08 +0530

    Added Build folder

---
+++ build/precache-manifest.98956f5566c20be869eab8057d542d0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da7c2fdca6bdeaa2a7a5716053cad6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bef2c647d4e4d52b3371",
+    "url": "/static/css/main.b27e6706.chunk.css"
+  },
+  {
+    "revision": "67f17baaad38138f2146",
+    "url": "/static/js/2.292ad4fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.292ad4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef2c647d4e4d52b3371",
+    "url": "/static/js/main.c4d36c5f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "347dd92181fc7cf3ca22117b2b73209e",
+    "url": "/static/media/housemain.347dd921.jpg"
+  }
+]);
